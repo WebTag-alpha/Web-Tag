aa590dcda064c3db7b5eb7bc20f11b4fde6ba2c9
WebTag-alpha <webtag.alpha@gmail.com>
2025-06-08 11:51:49 +0300

    Delete server directory

--- server/models/user.model.js
+++
@@ -1,8 +0,0 @@
-import mongoose, { Schema } from "mongoose";
-
-const userSchema = new Schema({
-  userWallet: { type: String, required: true },
-  userCID: String,
-});
-
-export const User = mongoose.model("User", userSchema);
